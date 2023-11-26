ce4cdcb7964fd4f4b2619cea3eea89ad5b45d615
hillary <hillary.saldanha199@gmail.com>
2023-11-26 04:33:46 -0300

    Correção das reservas de voos.

--- reserva.py
+++ reserva.py
@@ -9,25 +9,25 @@ def reservarVoo(voos, dadosVoo, reservas, reservante):
                 if id == 5410:
                     break
                 elif id < len(voos):
-                    print(f'+ Reserva do voo {voos[id]["Nome"]}: ')
-                    while True:
-                        passageiro = str(input('Digite seu nome para fazer a reserva: ')).strip()
-                        if passageiro == '':
-                            print('Não foi possível realizar sua reserva. Tente novamente!')
-                        else:
-                            reservante[f'{voos[id]["Nome"]}'] = passageiro
-                            reservas.append(reservante.copy())
-                            reservante.clear()
-                            #voos[id]['Reservantes'].append(reservante)
-                            #print(voos[id]['Reservantes'])
-                            print(f'Reserva realizada com sucesso no nome de {passageiro}')
-                            voos[id]['Reservas'] += 1
-                            voos[id]['Assentos'] -= 1
-                            break
+                    print(f'+ Reserva do voo {voos[id]["Código"]}: ')
+                    if voos[id]['Assentos'] > 0:
+                        while True:
+                            passageiro = str(input('Digite seu nome para fazer a reserva: ')).strip()
+                            if passageiro == '':
+                                print('Não foi possível realizar sua reserva. Tente novamente!')
+                            else:
+                                reservante[f'{voos[id]["Código"]}'] = passageiro
+                                reservas.append(reservante.copy())
+                                reservante.clear()
+                                print(f'Reserva realizada com sucesso no nome de {passageiro}')
+                                voos[id]['Reservas'] += 1
+                                voos[id]['Assentos'] -= 1
+                                mostrarVoos(voos, dadosVoo)
+                                break
+                    else: print('Assentos esgotados')
                 else:
                     print(f'ERRO! Não existe um voo com o ID {id}.')
                     print('=+' * 35) 
-                    id = int(input('Escolha um dos IDs para realizar a reserva do voo: '))
             except (ValueError, TypeError): 
                 print('Digite um número, por favor!')
                 continue
