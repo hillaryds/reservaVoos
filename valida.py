b9e3ef950a5df1553e157cce8eb2400a0f29cc51
hillary <hillary.saldanha199@gmail.com>
2023-11-26 23:56:25 -0300

    Adição docstrings.

--- valida.py
+++ valida.py
@@ -1,4 +1,10 @@
 def validaEmail(email, usuarios):
+    """
+    Valida o email cadastrado.
+    + param email - recebe o email cadastrado;
+    + param usuarios - recebe a lista de usuarios
+    + return - retorna True se o email for válido
+    """
     if email.count('@') == 1:
         if '.com' in email.split('@')[1] or '.org' in email.split('@')[1] or '.br' in email.split('@')[1]:
             if email.split('@')[0] != '' and email.split('@')[0] != ' ':
@@ -11,6 +17,13 @@ def validaEmail(email, usuarios):
 
 
 def validaUsuario( email, senha, usuarios):
+    """
+    Valida o usuário no login
+    + param email - recebe o email digitado no login;
+    + param senha - recebe a senha digitada no login;
+    + param usuarios - recebe a lista de usuarios;
+    + return - retorna verdadeiro se o usuário existir.
+    """
     for dadoUsuario in usuarios:
         if dadoUsuario['Email'] == email and dadoUsuario['Senha'] == senha:
             return(True)
